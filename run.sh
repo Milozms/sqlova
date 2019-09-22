53ca6dbd97dd0f078c6ffa1810336ffa977bea56
Miles <maosenzhang.milo@gmail.com>
2019-09-22 16:23:30 -0400

    fix cp

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@
 cd /home/maosen/sqlova
 module load anaconda/5.1.0-py36
 mkdir /home/maosen/sqlova/smalls/run-$RID
-cp /home/maosen/sqlova/data/wikisql_tok/train.* /home/maosen/sqlova/smalls/run-$RID
-cp /home/maosen/sqlova/data/wikisql_tok/test.* /home/maosen/sqlova/smalls/run-$RID
-cp /home/maosen/sqlova/data/wikisql_tok/dev.* /home/maosen/sqlova/smalls/run-$RID
+cp /home/maosen/sqlova/data/wikisql_tok/train* /home/maosen/sqlova/smalls/run-$RID
+cp /home/maosen/sqlova/data/wikisql_tok/test* /home/maosen/sqlova/smalls/run-$RID
+cp /home/maosen/sqlova/data/wikisql_tok/dev* /home/maosen/sqlova/smalls/run-$RID
 python split_data.py $SEED $RID
 python train-small.py --fine_tune --seed $SEED --run_id $RID --log_file log-small-constr-run-$RID
