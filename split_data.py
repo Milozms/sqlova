9cd619999298ace9f0d368f7e3339ee3ddbc1ced
Miles <maosenzhang.milo@gmail.com>
2019-09-22 16:00:38 -0400

    fix dir

--- split_data.py
+++ split_data.py
@@ -12,7 +12,7 @@ ratio = 0.1
 # os.system('cp ./data/wikisql_tok/train_tok.jsonl ./data/wikisql_tok/train_tok_full.jsonl')
 
 data = []
-with open('./data/wikisql_tok/train_tok_full.jsonl','r') as fin:
+with open('/home/maosen/data/wikisql_tok/train_tok_full.jsonl','r') as fin:
     for idx, line in enumerate(fin):
         t1 = json.loads(line.strip())
         data.append(t1)
@@ -21,6 +21,6 @@ dsize = int(len(data)*ratio)
 random.shuffle(data)
 data = data[:dsize]
 
-with open('./smalls/run-%d/train_tok.jsonl' % run_id,'w') as fout:
+with open('/home/maosen/smalls/run-%d/train_tok.jsonl' % run_id,'w') as fout:
     for d in data:
         fout.write(json.dumps(d) + '\n')
